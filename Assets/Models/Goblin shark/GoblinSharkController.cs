da8a38c083c72ce71502a3b2942a26fa8b8499cd
TimWaskett <tim.waskett@gmail.com>
2017-08-10 20:01:01 +0100

    Added goblin shark model and prepared the prefab

---
+++ Assets/Models/Goblin shark/GoblinSharkController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class GoblinSharkController : MonoBehaviour
+{
+    private Animator m_anim;
+    static int m_biteState = Animator.StringToHash("Bite Layer.Bite");
+
+    void Awake()
+    {
+        m_anim = GetComponent<Animator>();
+        //print(m_biteState);
+    }
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            var state = m_anim.GetCurrentAnimatorStateInfo(0);
+
+            //print(state.fullPathHash);
+
+            if (state.fullPathHash != m_biteState)
+                m_anim.SetTrigger("Bite");
+        }
+    }
+}
