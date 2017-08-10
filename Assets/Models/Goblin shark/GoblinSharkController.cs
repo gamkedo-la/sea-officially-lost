7048cbec6b216358db83607225278907bb7d6950
TimWaskett <tim.waskett@gmail.com>
2017-08-10 20:58:08 +0100

    Fixed the goblin shark animator script

--- Assets/Models/Goblin shark/GoblinSharkController.cs
+++ Assets/Models/Goblin shark/GoblinSharkController.cs
@@ -5,26 +5,41 @@ using UnityEngine;
 [RequireComponent(typeof(Animator))]
 public class GoblinSharkController : MonoBehaviour
 {
+    [SerializeField] int m_biteLayerIndex = 1;
+    [SerializeField] string m_biteStateName = "Bite";
+    [SerializeField] string m_biteTriggerName = "Bite";
+
     private Animator m_anim;
-    static int m_biteState = Animator.StringToHash("Bite Layer.Bite");
+    private string m_biteLayerName;
+    private int m_biteTriggerHash;
+    private int m_biteStateHash;
+
+    //private int m_baseLayerState;
+    //private int m_biteLayerState;
 
     void Awake()
     {
         m_anim = GetComponent<Animator>();
-        //print(m_biteState);
+        m_biteLayerName = m_anim.GetLayerName(m_biteLayerIndex);
+        m_biteStateHash = Animator.StringToHash(m_biteLayerName + "." + m_biteStateName);
+        m_biteTriggerHash = Animator.StringToHash(m_biteTriggerName);
+        //print(m_biteStateHash);
     }
 
 
     void Update()
     {
+        //m_baseLayerState = m_anim.GetCurrentAnimatorStateInfo(0).fullPathHash;
+        //m_biteLayerState = m_anim.GetCurrentAnimatorStateInfo(1).fullPathHash;
+
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            var state = m_anim.GetCurrentAnimatorStateInfo(0);
+            var state = m_anim.GetCurrentAnimatorStateInfo(m_biteLayerIndex);
 
             //print(state.fullPathHash);
 
-            if (state.fullPathHash != m_biteState)
-                m_anim.SetTrigger("Bite");
+            if (state.fullPathHash != m_biteStateHash)
+                m_anim.SetTrigger(m_biteTriggerHash);
         }
     }
 }
