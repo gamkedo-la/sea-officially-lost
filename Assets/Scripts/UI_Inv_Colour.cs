3f41c48bcb43456f82b7eef0abb4dc4c62c8592e
sergiosolorzano <sergiosolorzano@gmail.com>
2017-08-14 18:50:44 +0100

    player inventory wip

---
+++ Assets/Scripts/UI_Inv_Colour.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_Inv_Colour : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Image backgroundImage = GameObject.Find("PlayerInvBgImage").GetComponent<Image>();
+        backgroundImage.color = UnityEngine.Color.blue;
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
