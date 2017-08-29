66bd54d387a8c393e1f99ac4a7d876f37a16a62b
sergiosolorzano <sergiosolorzano@gmail.com>
2017-08-29 18:20:40 +0100

    inv ui wip

---
+++ Assets/Scripts/Inventory/InventoryMgr.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using System.Text;
+using System;
+
+public class InventoryMgr : MonoBehaviour
+{
+    bool invPanelToggle = false;
+    [SerializeField]
+    private Inventory inventoryList;
+    [SerializeField]
+    private GameObject inventoryPanel;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            invPanelToggle =! invPanelToggle;
+            if (invPanelToggle)
+                inventoryPanel.SetActive(true);
+            else inventoryPanel.SetActive(false);
+        }   
+    }
+}
