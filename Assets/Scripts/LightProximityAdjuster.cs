4308e6b5f515649cbf2a47645866485663e75d09
nick <polchiesnicholas@gmail.com>
2017-09-17 00:44:52 -0300

    Light and post processing for underwater scene. Renamed a few things
    Renamed character controller prefabs for brevity
    Added post processing to underwater scene
    Combined flashlight spotlights in underwater scene
    Created player prefab from underwater player controller
    Toned down underwater base spotlight
    Base spotlight brightens when distant to cut through fog

---
+++ Assets/Scripts/LightProximityAdjuster.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Light))]
+public class LightProximityAdjuster : MonoBehaviour {
+    private Light lamp;
+    public Transform distanceFrom;
+    private enum Mode {
+        Linear, Exponential, ExponentialSquared
+    };
+    [SerializeField] private Mode mode;
+    public float minIntensity, maxIntensity;
+    public float minDistance, maxDistance;
+
+    private void Start() {
+        lamp = GetComponent<Light>();
+    }
+
+    void Update () {
+        float intensity = CalculateIntensity();
+        lamp.intensity = Mathf.Lerp(minIntensity, maxIntensity, intensity);
+	}
+
+    private float CalculateIntensity() {
+        float t = Mathf.InverseLerp(minDistance, maxDistance, Vector3.Distance(transform.position, distanceFrom.position));
+//        if (mode == Mode.Linear) {
+            return t;
+/*       }
+        else if(mode == Mode.Exponential) {
+            return Mathf.Exp(t);
+        }
+        else {
+
+        }
+        return 0f;
+*/
+    }
+}
