e6f678b6a0f536d1a2aba6819038a937ffa18347
nick <polchiesnicholas@gmail.com>
2017-09-17 11:44:46 -0300

    Created Main Menu scene
    Non-functional first creation of main menu

---
+++ Assets/BreathingController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class BreathingController : MonoBehaviour {
+    public float breathTime;
+    private float breathClock;
+    private bool breathingIn;
+    private ParticleSystem bubbleSystem;
+
+	void Start () {
+        breathClock = 0f;
+        bubbleSystem = GetComponent<ParticleSystem>();
+	}
+	
+	void Update () {
+        breathClock += Time.deltaTime * (breathingIn ? 1 : -1);
+        if(breathClock > breathTime) {
+            //play breathing out sound for breathTime duration
+            bubbleSystem.Play();
+            breathingIn = false;
+        }
+        else if(breathClock < 0) {
+            breathingIn = true;
+        }
+	}
+}
