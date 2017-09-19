7fcdc7cac0e1350d4a717d65a5dce09ce2f9c528
nick <polchiesnicholas@gmail.com>
2017-09-19 16:58:08 -0300

    Main menu style improvements and submenus added
    Added options menu and video, audio, gameplay sub options menus
    Added underwater dust effect to main menu
    Added spotlight effect to main menu (fake volumetric light)
    Credit to Sehyun Av Kim's tutorial for the fake volumetric light code
    Minor asset organization (new folders/assets moved to folders)

--- Assets/Scripts/LightProximityAdjuster.cs
+++ Assets/Scripts/LightProximityAdjuster.cs
@@ -1,15 +1,9 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 [RequireComponent(typeof(Light))]
 public class LightProximityAdjuster : MonoBehaviour {
     private Light lamp;
     public Transform distanceFrom;
-    private enum Mode {
-        Linear, Exponential, ExponentialSquared
-    };
-    [SerializeField] private Mode mode;
     public float minIntensity, maxIntensity;
     public float minDistance, maxDistance;
 
@@ -18,22 +12,7 @@ public class LightProximityAdjuster : MonoBehaviour {
     }
 
     void Update () {
-        float intensity = CalculateIntensity();
+        float intensity = Mathf.InverseLerp(minDistance, maxDistance, Vector3.Distance(transform.position, distanceFrom.position));
         lamp.intensity = Mathf.Lerp(minIntensity, maxIntensity, intensity);
 	}
-
-    private float CalculateIntensity() {
-        float t = Mathf.InverseLerp(minDistance, maxDistance, Vector3.Distance(transform.position, distanceFrom.position));
-//        if (mode == Mode.Linear) {
-            return t;
-/*       }
-        else if(mode == Mode.Exponential) {
-            return Mathf.Exp(t);
-        }
-        else {
-
-        }
-        return 0f;
-*/
-    }
 }
---
+++ Assets/Scripts/Menus/MenuButtonBehavoir.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class MenuButtonBehavoir : MonoBehaviour, IPointerExitHandler {
+    public void OnPointerExit(PointerEventData eventData) {
+        EventSystem.current.SetSelectedGameObject(null);
+    }
+}
