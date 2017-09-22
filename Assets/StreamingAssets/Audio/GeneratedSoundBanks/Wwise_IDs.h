6112910b7da4997edb65998a183d056e01d7726e
M D Fewkes <mdfewkes@gmail.com>
2017-09-21 19:17:45 -0600

    Added Wwise event for the audio log test

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -13,6 +13,7 @@ namespace AK
 {
     namespace EVENTS
     {
+        static const AkUniqueID AUDIO_LOG_TEST = 1936257133U;
         static const AkUniqueID PLAY_FOOTSTEPS = 3854155799U;
         static const AkUniqueID PLAY_TEST_AMBI = 1234110288U;
     } // namespace EVENTS
