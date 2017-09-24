bb4aeca5f1dfa0c170ff5769f0da0c3c71c12567
M D Fewkes <mdfewkes@gmail.com>
2017-09-24 02:01:47 -0600

    Hooked Wwise syncs to options menu

---
+++ Assets/Scripts/WwiseSyncs.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WwiseSyncs : MonoBehaviour {
+
+	public void VolumeMaster(float vol) {
+		AkSoundEngine.SetRTPCValue("Volume_Master", vol);
+	}
+
+	public void VolumeMusic(float vol) {
+		AkSoundEngine.SetRTPCValue("Volume_Music", vol);
+	}
+
+	public void VolumeSFX(float vol) {
+		AkSoundEngine.SetRTPCValue("Volume_SFX", vol);
+	}
+
+}
