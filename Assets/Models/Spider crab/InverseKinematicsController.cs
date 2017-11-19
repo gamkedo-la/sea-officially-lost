678fe507e9b759ed0a833d37625a60d8c23202aa
TimWaskett <tim.waskett@gmail.com>
2017-11-19 15:02:39 +0000

    Tinkering with a procedural animation system for the crab claws.

---
+++ Assets/Models/Spider crab/InverseKinematicsController.cs
@@ -0,0 +1,133 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InverseKinematicsController : MonoBehaviour
+{
+    [SerializeField] Transform m_target;
+    [SerializeField] RobotJoint[] Joints;
+    [SerializeField] float SamplingDistance = 0.01f;
+    [SerializeField] float DistanceThreshold = 0.1f;
+    [SerializeField] float LearningRate = 0.1f;
+
+    private Vector3[] angles;
+
+
+    void Update()
+    {
+        if (m_target != null)
+        {
+            angles = new Vector3[Joints.Length];
+
+            for (int i = 0; i < angles.Length; i++)
+                angles[i] = Joints[i].transform.localEulerAngles;
+
+            InverseKinematics(m_target.position, angles);
+
+            for (int i = 0; i < Joints.Length; i++)
+            {
+                var joint = Joints[i];
+                var angle = angles[i]; 
+                joint.transform.localRotation = Quaternion.Euler(angle);
+            }
+        }
+    }
+
+
+    private Vector3 ForwardKinematics(Vector3[] angles)
+    {
+        Vector3 prevPoint = Joints[0].transform.position;
+
+        Quaternion rotation = Quaternion.identity;
+        for (int i = 1; i < Joints.Length; i++)
+        {
+            // Rotates around a new axis
+            rotation *= Quaternion.Euler(angles[i - 1]);
+            //rotation *= Quaternion.AngleAxis(angles[i - 1], Joints[i - 1].Axis);
+            Vector3 nextPoint = prevPoint + rotation * Joints[i].StartOffset;
+
+            prevPoint = nextPoint;
+        }
+
+        return prevPoint;
+    }
+
+
+    private float DistanceFromTarget(Vector3 target, Vector3[] angles)
+    {
+        Vector3 point = ForwardKinematics(angles);
+        return Vector3.Distance(point, target);
+    }
+
+
+    private float PartialGradient(Vector3 target, Vector3[] angles, int i)
+    {
+        // Saves the angle,
+        // it will be restored later
+        var angle = angles[i];
+
+        // Gradient : [F(x+SamplingDistance) - F(x)] / h
+        float f_x = DistanceFromTarget(target, angles);
+
+        angles[i] += SamplingDistance * Joints[i].Axis;
+        float f_x_plus_d = DistanceFromTarget(target, angles);
+
+        float gradient = (f_x_plus_d - f_x) / SamplingDistance;
+
+        // Restores
+        angles[i] = angle;
+
+        return gradient;
+    }
+
+
+    private void InverseKinematics(Vector3 target, Vector3[] angles)
+    {
+        if (DistanceFromTarget(target, angles) < DistanceThreshold)
+            return;
+
+        for (int i = Joints.Length - 1; i >= 0; i--)
+        {
+            // Gradient descent
+            // Update : Solution -= LearningRate * Gradient
+            float gradient = PartialGradient(target, angles, i);
+            angles[i] -= Joints[i].Axis * LearningRate * gradient;
+
+            // Clamp
+            float axisAngle = AxisAngle(angles[i], Joints[i].Axis);
+            axisAngle = Mathf.Clamp(axisAngle, Joints[i].MinAngle, Joints[i].MaxAngle);
+            angles[i] = SetAxisAngle(axisAngle, angles[i], Joints[i].Axis);
+
+            // Early termination
+            if (DistanceFromTarget(target, angles) < DistanceThreshold)
+                return;
+        }
+    }
+
+
+    private float AxisAngle(Vector3 rotation, Vector3 axis)
+    {
+        var angle = Vector3.Scale(rotation, axis).magnitude;
+
+        return angle;
+    }
+
+
+    private Vector3 SetAxisAngle(float axisAngle, Vector3 angle, Vector3 axis)
+    {
+        var angleAlongAxis = axis * axisAngle;
+        var otherAngles = Vector3.Scale(Vector3.one - axis, angle);
+
+        //print("Initial angle: " + angle);
+        //print("axisAngle: " + axisAngle);
+        //print("axis:" + axis);
+        //print("angleAlongAxis: " + angleAlongAxis);
+        //print("otherAngles: " + otherAngles);
+
+        angle = angleAlongAxis + otherAngles;
+
+        //print("Final angle: " + angle);
+
+        return angle;
+    }
+}
---
+++ Assets/Models/Spider crab/RobotJoint.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RobotJoint : MonoBehaviour
+{
+    public Vector3 Axis;
+    public Vector3 StartOffset;
+
+    public float MinAngle = -360f;
+    public float MaxAngle = 360f;
+
+    void Awake()
+    {
+        StartOffset = Vector3.Scale(transform.localPosition, transform.lossyScale);
+    }
+}
