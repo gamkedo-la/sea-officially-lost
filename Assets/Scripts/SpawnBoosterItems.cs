51a330c9b16b4afd032e37fb0c0600f7ce8e38d9
Jeremy Kenyon <lanthos@gmail.com>
2017-12-17 22:25:33 -0800

    If player already has booster items in inventory, they won't show up each time they go back into the base.

---
+++ Assets/Scripts/SpawnBoosterItems.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnBoosterItems : MonoBehaviour {
+
+    public GameObject oxygenBooster;
+    public GameObject swimSpeedBooster;
+
+    void Start() {
+        InventoryMgr tempIM = GameObject.FindWithTag("InventoryManager").GetComponent<InventoryMgr>();
+
+        if (tempIM != null) {
+            if (tempIM.HasOxygen()) {
+                oxygenBooster.SetActive(false);
+            } else {
+                oxygenBooster.SetActive(true);
+            }
+
+            if (tempIM.HasSwimSpeedBoost()) {
+                swimSpeedBooster.SetActive(false);
+            } else {
+                swimSpeedBooster.SetActive(true);
+            }
+        }
+    }
+}
