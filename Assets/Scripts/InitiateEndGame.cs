5d2d684c9531d80d5044ef6e616c2a2f69752e0b
Jeremy Kenyon <lanthos@gmail.com>
2017-12-17 22:03:35 -0800

    Setup end game hooks (portal appears when you pick up the jewelry box), activating the portal brings you to win scene, setup various creature zones on the way to and from the wreck, moved around pills and speed boost in base.

---
+++ Assets/Scripts/InitiateEndGame.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InitiateEndGame : MonoBehaviour {
+
+    public GameObject portalObject;
+
+    public void ClickAction()
+    {
+        portalObject.SetActive(true);
+    }
+}
---
+++ Assets/Scripts/YouWon.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class YouWon : MonoBehaviour {
+
+    public string sceneToLoad;
+    public void ClickAction()
+    {
+        SceneManager.LoadScene(sceneToLoad);
+    }
+}
