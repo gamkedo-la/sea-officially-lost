e3e3ab9cc9e500cb0f95395108206248e131dc7a
Chris DeLeon <gamkedo@gmail.com>
2018-05-05 09:20:19 -0700

    Adding Escape key to quit from builds (in PlayerCommon, assuming it's in both scenes, currently building to test but build is taking a looooong time :)

--- Assets/Scripts/PlayerCommon.cs
+++ Assets/Scripts/PlayerCommon.cs
@@ -47,6 +47,10 @@ public class PlayerCommon : MonoBehaviour {
     // Update is called once per frame
     void Update () {
 
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
 		if (Input.GetKeyDown(KeyCode.Minus))
 		{
 			ReleaseMouse();
